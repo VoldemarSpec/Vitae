6a416da9467d328a66f7f1dd76915ebbe5ce78af
Volodymyr Spetsialnyi <volodymirspecialnyi@gmail.com>
2026-08-12 23:20:21 +0200

    feat: added chat router

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+from Vitae.app.backend.routes import chat
 
 app = FastAPI()
 
-
+app.include_router(chat.router, prefix="/api/v1/chat", tags=["chat"])
 
 
 @app.get("/")
 async def root():
     return {"message": "backend is running"}
-
 
 
 if __name__ == "__main__":
