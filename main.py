6b51129227a5c6ab8c81a47b9055c0a67f375d06
Volodymyr Spetsialnyi <volodymirspecialnyi@gmail.com>
2026-08-13 15:41:50 +0200

    feat: added cors

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from Vitae.app.backend.routes import chat
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat.router, prefix="/api/v1/chat", tags=["chat"])
 
