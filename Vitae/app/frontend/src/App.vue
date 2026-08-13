94acd60dcf57e069a515a9896b52b53b734f11b2
Volodymyr Spetsialnyi <volodymirspecialnyi@gmail.com>
2026-08-13 15:35:46 +0200

    chore: tailwind setup

--- Vitae/app/frontend/src/App.vue
+++ Vitae/app/frontend/src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div
+    class="min-h-screen bg-slate-900 text-white flex items-center justify-center"
+  >
+    <h1 class="text-3xl font-bold text-sky-400">Tailwind Check</h1>
+  </div>
 </template>
