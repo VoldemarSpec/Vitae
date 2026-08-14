aa88d860b80dd7864e0b663e277095aac04ce79f
Volodymyr Spetsialnyi <volodymirspecialnyi@gmail.com>
2026-08-14 13:49:03 +0200

    feat: exceptions added

--- Vitae/app/externalservices/openai/openai_api.py
+++ Vitae/app/externalservices/openai/openai_api.py
@@ -3,18 +3,27 @@ import asyncio
 from openai import DefaultAioHttpClient
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
-load_dotenv() 
 
-async def send_message(messages:list) -> str:
-    async with AsyncOpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),  
-        http_client=DefaultAioHttpClient(),
-    ) as client:
-        chat_completion = await client.chat.completions.create(
-            messages = messages,
-            model="gpt-4o-mini",
-            temperature=0.7
-        )
-        bot_reply = chat_completion.choices[0].message.content
-        return bot_reply
+load_dotenv()
 
+logger = logging.getLogger(__name__)
+
+
+class OpenAIIntegrationError(Exception):
+    pass
+
+
+async def send_message(messages: list) -> str:
+    try:
+        async with AsyncOpenAI(
+            api_key=os.getenv("OPENAI_API_KEY"),
+            http_client=DefaultAioHttpClient(),
+        ) as client:
+            chat_completion = await client.chat.completions.create(
+                messages=messages, model="gpt-4o-mini", temperature=0.7
+            )
+            return chat_completion.choices[0].message.content
+
+    except Exception as e:
+        logger.error(f"Failed to connect to OpenAI: {e}")
+        raise OpenAIIntegrationError("OpenAI service is unavailable.")
